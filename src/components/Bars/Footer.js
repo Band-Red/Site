1d6ca7e814ca5bd79ed66c5344fb5676d34427d9
FAHAD <fahad@example.com>
2025-11-04 19:47:34 +0300

    Task 3: Finsh from  make section Contact responsive with large Devices.

--- src/components/Bars/Footer.js
+++ src/components/Bars/Footer.js
@@ -2,12 +2,12 @@
 export default function Footer() {
   return (
     <footer>
-      All rights reserved at 
-      <a href={'https://github.com/Band-Red'} target={'_blank'} rel={'noopener'}>
-        Band-Red
-      </a> 
-        2025 
       <sup title={'copy-right'}>©</sup>
+        2025 
+      <a href={'https://github.com/Band-Red'} target={'_blank'} rel={'noopener'}>
+        Band-Red.
+      </a> 
+      All rights reserved
     </footer>
   )
 }
